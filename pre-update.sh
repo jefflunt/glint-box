c15f3cae0ab198d6d980efc6039ffb3551f84d36
Jeff Lunt <jefflunt@gmail.com>
2012-09-02 07:46:15 -0500

    Reordered one of the commands. Fixed a bug in the repository echoing that was preventing the location of the RetroArch binaries from being available.

--- pre-update.sh
+++ pre-update.sh
@@ -2,10 +2,10 @@
 
 sudo wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update && chmod +x /usr/bin/rpi-update
 sudo apt-get install ca-certificates
+sudo rpi-update
 
 # From http://superpiadventures.com/2012/08/retroarch-packages-for-raspbian/#comment-48
 
-sudo echo "deb http://archive.changeover.za.net/raspbian wheezy main" > /etc/apt/sources.list.d/changeover.list
+echo "deb http://archive.changeover.za.net/raspbian wheezy main" | sudo tee /etc/apt/sources.list.d/changeover.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 2747C7CF
 sudo apt-get update; sudo apt-get dist-upgrade
-sudo rpi-update

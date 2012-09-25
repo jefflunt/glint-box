0c9cf7371c9050bed23ff5461a55bdc482681c00
Jeff Lunt <jefflunt@gmail.com>
2012-09-24 21:33:19 -0500

    Updated pre- and post- shell scripts to (1) remove the Raspberry Pi firmware update, which is no longer required nor available, and (2) to install the joystick binaries and run the configuration app.

--- post-update.sh
+++ post-update.sh
@@ -1,3 +1,15 @@
+# Install joystick/joypad binaries
+
+sudo apt-get -y install joystick
+js
+
+echo ""
+echo ""
+echo "***** We're now going to configure your controller."
+echo ""
+read -p "***** PLUG IT IN NOW and press [Enter] to continue..."
+jscal -c /dev/input/js0
+
 # Install RetroArch binaries
 
 sudo apt-get -y update; sudo apt-get -y install libretro-fceu retroarch retroarch-phoenix
--- pre-update.sh
+++ pre-update.sh
@@ -1,11 +1,7 @@
-# Download the `rpi-update` script and update the root ca-certificates
-
-sudo wget http://goo.gl/1BOfJ -O /usr/bin/rpi-update && chmod +x /usr/bin/rpi-update
+# Update the root ca-certificates
 sudo apt-get -y install ca-certificates
-sudo rpi-update
 
-# From http://superpiadventures.com/2012/08/retroarch-packages-for-raspbian/#comment-48
-
+# Download retroarch binaries - from http://superpiadventures.com/2012/08/retroarch-packages-for-raspbian/#comment-48
 echo "deb http://archive.changeover.za.net/raspbian wheezy main" | sudo tee /etc/apt/sources.list.d/changeover.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 2747C7CF
 sudo apt-get -y update; sudo apt-get -y dist-upgrade

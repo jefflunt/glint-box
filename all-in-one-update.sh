303fb0d5760ada72c8ab310541e4316ea3d93dc3
Jeff Lunt <jefflunt@gmail.com>
2012-09-24 21:38:56 -0500

    Updated the README to use the experimental "all-in-one" update shell script, which is essentially a shell script that should do all the combined updates in a single run now. Also added instructions to reboot and what command to run after the reboot.

---
+++ all-in-one-update.sh
@@ -0,0 +1,37 @@
+# Update the root ca-certificates
+sudo apt-get -y install ca-certificates
+
+# Download retroarch binaries - from http://superpiadventures.com/2012/08/retroarch-packages-for-raspbian/#comment-48
+echo "deb http://archive.changeover.za.net/raspbian wheezy main" | sudo tee /etc/apt/sources.list.d/changeover.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 2747C7CF
+sudo apt-get -y update; sudo apt-get -y dist-upgrade
+
+# Install joystick/joypad binaries
+
+sudo apt-get -y install joystick
+js
+
+echo ""
+echo ""
+echo "***** We're now going to configure your controller."
+echo ""
+read -p "***** PLUG IT IN NOW and press [Enter] to continue..."
+jscal -c /dev/input/js0
+
+# Install RetroArch binaries
+
+sudo apt-get -y update; sudo apt-get -y install libretro-fceu retroarch
+
+# Create config file for running RetroArch
+
+mkdir -p ~/.config/retroarch/
+cp retroarch.cfg ~/.config/retroarch/
+
+echo ""
+echo ""
+echo "***** We will now reboot. But when we come back, run the following command to start retroarch."
+echo "retroarch <filename.nes> -c ~/.config/retroarch/retroarch.cfg -v"
+echo ""
+read -p "***** Press [Enter] to reboot..."
+
+sudo shutdown -r now
--- post-update.sh
+++ post-update.sh
@@ -19,6 +19,11 @@ sudo apt-get -y update; sudo apt-get -y install libretro-fceu retroarch retroarc
 mkdir -p ~/.config/retroarch/
 cp retroarch.cfg ~/.config/retroarch/
 
+# Create config file for running RetroArch
+
+mkdir -p ~/.config/retroarch/
+cp retroarch.cfg ~/.config/retroarch/
+
 echo ""
 echo "Now run it!:"
 echo "retroarch <filename.nes> -c ~/.config/retroarch/retroarch.cfg -v"

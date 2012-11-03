25641a8670ea53c7eeb815addb1a20cca1919a4d
Jeff Lunt <jefflunt@gmail.com>
2012-11-03 10:38:10 -0500

    Removed the distribution upgrade option.

--- all-in-one-update.sh
+++ all-in-one-update.sh
@@ -6,7 +6,11 @@ sudo apt-get -y install ca-certificates
 # Download retroarch binaries - from http://superpiadventures.com/2012/08/retroarch-packages-for-raspbian/#comment-48
 echo "deb http://archive.changeover.za.net/raspbian wheezy main" | sudo tee /etc/apt/sources.list.d/changeover.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 2747C7CF
-sudo apt-get -y update; sudo apt-get -y dist-upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# Temporarily commented out - might discard at some point
+#sudo apt-get -y dist-upgrade
 
 # Install joystick/joypad binaries
 

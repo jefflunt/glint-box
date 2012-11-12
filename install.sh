a12b6d58093c523bed6c327c7535f9a553d232d0
Jeff Lunt <jefflunt@gmail.com>
2012-11-12 08:13:59 -0600

    Altered the install script and added the modules file based on last night's testing. Should have much improved audio support and more head room with RAM, resulting in better performance for many games and fewer audio issues overall.

--- install.sh
+++ install.sh
@@ -1,50 +1,57 @@
 #!/bin/bash
 
 # Update pi user's profile, Raspberry Pi config file, and auto-login inittab
-curl -L https://raw.github.com/normalocity/glint-nes/master/.profile > ~/.profile
-curl -L https://raw.github.com/normalocity/glint-nes/master/raspberry-pi-config.txt > /tmp/config.txt
-sudo mv /boot/config.txt /boot/config.txt.backup
-sudo mv /tmp/config.txt /boot/config.txt
+curl -L https://raw.github.com/normalocity/glint-nes/wheezy-min/.profile > ~/.profile
+curl -L https://raw.github.com/normalocity/glint-nes/wheezy-min/raspberry-pi-config.txt > /tmp/config.txt
+mv /boot/config.txt /boot/config.txt.backup
+mv /tmp/config.txt /boot/config.txt
 
-curl -L https://raw.github.com/normalocity/glint-nes/master/inittab > /tmp/inittab
-sudo mv /etc/inittab /etc/inittab.backup
-sudo mv /tmp/inittab /etc/inittab
+curl -L https://raw.github.com/normalocity/glint-nes/wheezy-min/inittab > /tmp/inittab
+mv /etc/inittab /etc/inittab.backup
+mv /tmp/inittab /etc/inittab
 
 # Update the root ca-certificates and the already installed packages
-sudo apt-get -y install ca-certificates
+apt-get -y install ca-certificates libxv1
 
 # Download retroarch binaries - from http://superpiadventures.com/2012/08/retroarch-packages-for-raspbian/#comment-48
-echo "deb http://archive.changeover.za.net/raspbian wheezy main" | sudo tee /etc/apt/sources.list.d/changeover.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 2747C7CF
+echo "deb http://archive.changeover.za.net/raspbian wheezy main" | tee /etc/apt/sources.list.d/changeover.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key 2747C7CF
 
 # DON'T move this line to anyplace BEFORE the addition of the apt-key
-sudo apt-get -y update
-sudo apt-get -y upgrade
+apt-get -y update
+apt-get -y upgrade
 # end DON'T
 
 # Install RetroArch binaries
-sudo apt-get -y install libretro-fceu retroarch
+apt-get -y install libretro-fceu retroarch
 
 # Custom splash screen stuff - disabled for now because it's unstable
 # From: http://raspberrypi.stackexchange.com/questions/1214/how-to-add-custom-loading-screen
-# sudo apt-get -y install fbi
-# curl -L https://raw.github.com/normalocity/glint-nes/master/glint-nes-splash.png > /tmp/splash.png
-# sudo mv /tmp/splash.png > /etc/splash.png
+# apt-get -y install fbi
+# curl -L https://raw.github.com/normalocity/glint-nes/wheezy-min/glint-nes-splash.png > /tmp/splash.png
+# mv /tmp/splash.png > /etc/splash.png
 # 
-# curl -L https://raw.github.com/normalocity/glint-nes/master/glint-splash > /tmp/glint-splash
-# sudo mv /tmp/glint-splash /etc/init.d/aaasplash
+# curl -L https://raw.github.com/normalocity/glint-nes/wheezy-min/glint-splash > /tmp/glint-splash
+# mv /tmp/glint-splash /etc/init.d/aaasplash
 # 
-# sudo chmod a+x /etc/init.d/aaasplash
-# sudo insserv /etc/init.d/aaasplash
+# chmod a+x /etc/init.d/aaasplash
+# insserv /etc/init.d/aaasplash
 
 # Create config file for running RetroArch
 mkdir -p ~/.config/retroarch/
 cd ~/.config/retroarch/
-curl -L https://raw.github.com/normalocity/glint-nes/master/retroarch.cfg > retroarch.cfg
+curl -L https://raw.github.com/normalocity/glint-nes/wheezy-min/retroarch.cfg > retroarch.cfg
+
+# Setup audio module
+curl -L https://raw.github.com/normalocity/glint-nes/wheezy-min/modules > /etc/modules
+
+# Set 192/64 memory split
+mv /boot/start.elf /boot/start.elf.backup
+cp /boot/arm192_start.elf /boot/start.elf
 
 # Cleanup after yourself
-sudo apt-get autoremove
-sudo apt-get autoclean
+apt-get autoremove
+apt-get autoclean
 
 # Reboot
-sudo shutdown -r now
+shutdown -r now

ab68931b9e2547b69f48d5de8f3410b012954031
Jeff Lunt <jefflunt@gmail.com>
2013-01-05 20:54:29 -0600

    Changed the order of killing `glint-es` back to the last thing because the debug output I was expecting didn't work.

--- scripts/copy-roms.sh
+++ scripts/copy-roms.sh
@@ -10,13 +10,12 @@ if [ -d "$TARGET" ]; then
 fi
 if [ -d "$SOURCE" ]; then
   # Stop glint-es, clear console, and start ROM file copy
-  echo "Copying ROMs from USB stick...please wait..."
   find $SOURCE -type f \( -name "*.nes" -o -name "*.NES" -o -name "*.Nes" \) -a \( ! -regex '.*/\..*' \) -exec cp {} $TARGET \; 
   sudo chown -R pi:pi $HOME/roms
+  echo "Copied ROMs to $TARGET" >> /var/log/syslog
   
   # Unmount USB stick and restart glint-es
   sudo umount /mnt/removeable
-  echo "Starting glint-es"
   killall glint-es && /home/pi/glint-es/glint-es &
 else
    echo "ERR: $SOURCE does not exist."   

4be3b895ccea59518b8c6579aa1e41913a48f81b
Jeff Lunt <jefflunt@gmail.com>
2013-01-05 20:47:03 -0600

    Modified the `copy-roms.sh` script to clean up some things, and to provide a little more feedback to the user while the copying is happening.

--- scripts/copy-roms.sh
+++ scripts/copy-roms.sh
@@ -9,11 +9,17 @@ if [ -d "$TARGET" ]; then
    rm -rf $TARGET/*
 fi
 if [ -d "$SOURCE" ]; then
-   find $SOURCE -type f \( -name "*.nes" -o -name "*.NES" -o -name "*.Nes" \) -a \( ! -regex '.*/\..*' \) -exec cp {} $TARGET \; 
-   echo "Copied ROMs to $TARGET" >> /var/log/syslog
-   sudo chown -R pi:pi $HOME/roms
-   killall glint-es && /home/pi/glint-es/glint-es &
-   sudo umount /mnt/removeable
+  # Stop glint-es, clear console, and start ROM file copy
+  killall glint-es
+  clear
+  echo "Copying ROMs from USB stick...please wait..."
+  find $SOURCE -type f \( -name "*.nes" -o -name "*.NES" -o -name "*.Nes" \) -a \( ! -regex '.*/\..*' \) -exec cp {} $TARGET \; 
+  sudo chown -R pi:pi $HOME/roms
+  
+  # Unmount USB stick and restart glint-es
+  sudo umount /mnt/removeable
+  echo "Starting glint-es"
+  /home/pi/glint-es/glint-es &
 else
    echo "ERR: $SOURCE does not exist."   
    exit 1

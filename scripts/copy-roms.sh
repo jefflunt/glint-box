2e89a3f5bf92397801da26ff6823da3af7dfa356
Jeff Lunt <jefflunt@gmail.com>
2013-01-05 20:35:52 -0600

    Modified auto-mount script to do the following: - Correct a permissions issues whereby the ROMs would be set to be owned by "root" instead of "pi" - Unmounted the USB stick after copying.

--- scripts/copy-roms.sh
+++ scripts/copy-roms.sh
@@ -11,7 +11,9 @@ fi
 if [ -d "$SOURCE" ]; then
    find $SOURCE -type f \( -name "*.nes" -o -name "*.NES" -o -name "*.Nes" \) -a \( ! -regex '.*/\..*' \) -exec cp {} $TARGET \; 
    echo "Copied ROMs to $TARGET" >> /var/log/syslog
+   sudo chown -R pi:pi $HOME/roms
    killall glint-es && /home/pi/glint-es/glint-es &
+   sudo umount /mnt/removeable
 else
    echo "ERR: $SOURCE does not exist."   
    exit 1

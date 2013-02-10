c89d05d36d0a58b3b0fb29f826d77774dd2a0a3d
normalocity <jefflunt@gmail.com>
2013-02-10 16:41:38 -0600

    Re-ordered the controls list to match that of the inputs.

--- scripts/retroarch-controls-rewrite.sh
+++ scripts/retroarch-controls-rewrite.sh
@@ -12,11 +12,12 @@ declare -A buttons
 buttons=( ["up"]="$3" ["down"]="$4" ["left"]="$5" ["right"]="$6" ["b"]="$7" ["a"]="$8" ["select"]="$9" ["start"]="$10" )
 
 sed -ie "s/input_player${PLAYER}_joypad_index.*/input_player${PLAYER}_joypad_index = \"${PLAYERINDEX}\"/g" $RACONFIG
-sed -ie "s/input_player${PLAYER}_a_btn.*/input_player${PLAYER}_a_btn = \"${buttons["a"]}\"/g" $RACONFIG
-sed -ie "s/input_player${PLAYER}_b_btn.*/input_player${PLAYER}_b_btn = \"${buttons["b"]}\"/g" $RACONFIG
-sed -ie "s/input_player${PLAYER}_select_btn.*/input_player${PLAYER}_select_btn = \"${buttons["select"]}\"/g" $RACONFIG
-sed -ie "s/input_player${PLAYER}_start_btn.*/input_player${PLAYER}_start_btn = \"${buttons["start"]}\"/g" $RACONFIG
-sed -ie "s/input_player${PLAYER}_left_axis.*/input_player${PLAYER}_left_axis = \"-${buttons["left"]}\"/g" $RACONFIG
-sed -ie "s/input_player${PLAYER}_right_axis.*/input_player${PLAYER}_right_axis = \"+${buttons["right"]}\"/g" $RACONFIG
+
 sed -ie "s/input_player${PLAYER}_up_axis.*/input_player${PLAYER}_up_axis = \"-${buttons["up"]}\"/g" $RACONFIG
 sed -ie "s/input_player${PLAYER}_down_axis.*/input_player${PLAYER}_down_axis = \"+${buttons["down"]}\"/g" $RACONFIG
+sed -ie "s/input_player${PLAYER}_left_axis.*/input_player${PLAYER}_left_axis = \"-${buttons["left"]}\"/g" $RACONFIG
+sed -ie "s/input_player${PLAYER}_right_axis.*/input_player${PLAYER}_right_axis = \"+${buttons["right"]}\"/g" $RACONFIG
+sed -ie "s/input_player${PLAYER}_b_btn.*/input_player${PLAYER}_b_btn = \"${buttons["b"]}\"/g" $RACONFIG
+sed -ie "s/input_player${PLAYER}_a_btn.*/input_player${PLAYER}_a_btn = \"${buttons["a"]}\"/g" $RACONFIG
+sed -ie "s/input_player${PLAYER}_select_btn.*/input_player${PLAYER}_select_btn = \"${buttons["select"]}\"/g" $RACONFIG
+sed -ie "s/input_player${PLAYER}_start_btn.*/input_player${PLAYER}_start_btn = \"${buttons["start"]}\"/g" $RACONFIG
